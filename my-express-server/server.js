59a8de19d730ba310700a6b7b97b2d5f304d2ad7
Yigithan Guven <demirejderha@gmail.com>
2021-08-07 19:40:34 +0300

    basic_Calculator

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,10 +2,21 @@
 const express = require("express");
 const app = express();
 
-app.get("/",function(request,response){
-   response.send("<h1>Hello world<h1>");
+app.get("/",function(req,res){
+   res.send("<h1>Hello world<h1>");
 });
+app.get("/contact",function(req,res){
+    res.send("contact me at yigithanguvenn@gmail.com");
+ });
+
+ app.get("/about",function(req,res){
+    res.send("23 year old dude");
+ });
+
+ app.get("/hobbies",function(req,res){
+    res.send("f1 and ipad");
+ });
 
 app.listen(3000,function(){
-    console.log("server started on prt 3000");
+    console.log("server started on port 3000");
 });
