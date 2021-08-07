1ec91eb7080291238e2a58c1e25d8773868c8c40
Yigithan Guven <demirejderha@gmail.com>
2021-08-07 20:01:12 +0300

    BMI calculator added

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function (req,res) {
-    res.sendFile(__dirname+"/index.html");
+    res.sendFile(__dirname+"/bmiCalculator.html");
     
 });
 app.post("/",function (req,res) {
@@ -14,6 +14,13 @@ app.post("/",function (req,res) {
 
     var result= num1+num2;
     res.send("The result of the calculation is "+result);
+})
+app.post("/bmiCalculator.html",function (req,res) {
+    var weight= Number(req.body.weight);
+    var height= Number(req.body.height);
+
+    var BMI= weight/(height*height);
+    res.send("Your BMI is --> "+BMI);
 })
 app.listen(3000,function () {
     console.log("server is running on 3000 port");
