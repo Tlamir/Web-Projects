ca74c8c12581e15dbb49a46aa03fdc786a18344e
Yigithan Guven <demirejderha@gmail.com>
2021-08-16 22:15:01 +0300

    Blogpost challange finished

--- EJS Challenge_Blog_Website/ejs-challenge/app.js
+++ EJS Challenge_Blog_Website/ejs-challenge/app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
-let posts=[];
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _= require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -20,12 +21,11 @@ app.use(express.static("public"));
 
 
 
-
+let posts=[];
 
 
 app.get("/", (req, res) => {
-  res.render("home",{startingContent :homeStartingContent });
-  console.log(posts);
+  res.render("home",{startingContent :homeStartingContent,posts:posts});
 })
 
 app.get("/about", (req, res) => {
@@ -40,6 +40,20 @@ app.get("/contact", (req, res) => {
 app.get("/compose", (req, res) => {
   res.render("compose");
 })
+
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle= _.lowerCase(req.params.postName);
+  posts.forEach(post => {
+    const storedTitle= _.lowerCase(post.title);
+    if (storedTitle===requestedTitle) {
+      console.log("Match");
+      res.render("post",{post :post });
+    } else{
+      console.log("Not a Match");
+    }
+  });
+  console.log(req.params.postName);
+});
 
 app.post('/compose', function (req, res) {
  const post = {title:req.body.postTitle, content:req.body.postBody};
